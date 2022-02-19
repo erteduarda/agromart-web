e6de6dff4a72bcd6e2e2f13282ecade5b44e1f31
IgorVeludo2 <velvet.guimaraes@gmail.com>
2022-02-19 16:11:07 -0300

    Importando react adm para a pagina inicial da aplicacao

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import logoImg  from '../../assets/logo.svg'
 import { Container, Content } from './styles'
+import { Admin } from 'react-admin'
+import jsonServerProvider from 'ra-data-json-server'
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 export function Header(){
   return (
-    <Container>
-      <Content>
-        <img src={ logoImg } alt="agromart"/>
-        Header
-      </Content>
-    </Container>
+    <>
+      <Container>
+        <Content>
+          <img src={ logoImg } alt="agromart"/>
+        </Content>
+      </Container>
+      <Admin dataProvider={ dataProvider }/>
+    </>
   )
 }
