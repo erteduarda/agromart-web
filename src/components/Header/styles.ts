ae167fb17a8683b30d65edc7171bdf7e73ab0028
IgorVeludo2 <velvet.guimaraes@gmail.com>
2022-02-19 15:54:08 -0300

    Criando tela inicial do projeto

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background: var(--green);
+`;
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+
+  padding: 0 1rem 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
